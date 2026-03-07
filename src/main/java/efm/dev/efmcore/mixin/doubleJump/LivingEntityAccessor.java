af18a6c57c03df8b098a8381ac2b0a0131dfdc99
Eyeofflame643 <570256661@qq.com>
2026-03-07 12:23:39 +0800

    修复了一个mixin bug

--- src/main/java/efm/dev/efmcore/mixin/doubleJump/LivingEntityAccessor.java
+++ src/main/java/efm/dev/efmcore/mixin/doubleJump/LivingEntityAccessor.java
@@ -1,14 +1,16 @@
 package efm.dev.efmcore.mixin.doubleJump;
 
+import net.minecraft.world.entity.Attackable;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.common.extensions.IForgeLivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(LivingEntity.class)
-public interface LivingEntityAccessor {
-    @Accessor("noJumpDelay")
+@Mixin(value = LivingEntity.class, remap = true)
+public interface LivingEntityAccessor extends Attackable, IForgeLivingEntity {
+    @Accessor(value = "noJumpDelay", remap = true)
     int getNoJumpDelay();
 
-    @Accessor("noJumpDelay")
+    @Accessor(value = "noJumpDelay", remap = true)
     void setNoJumpDelay(int delay);
 }
